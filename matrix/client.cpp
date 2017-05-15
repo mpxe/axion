ccfcef7eee18354080e85aa05879a09d142d104c
peter <xe@kthx.de>
2017-05-15 23:49:09 +0200

    Fix formatting

--- matrix/client.cpp
+++ matrix/client.cpp
@@ -6,15 +6,19 @@
 
 namespace
 {
-  matrix::Room* find_room(std::vector<matrix::Room>& rooms, std::string_view id)
-  {
-    auto it = std::find_if(std::begin(rooms), std::end(rooms), [id](const auto& r){
-      return r.id() == id;
-    });
-    if (it != std::end(rooms))
-      return &*it;
-    return nullptr;
-  }
+
+
+matrix::Room* find_room(std::vector<matrix::Room>& rooms, std::string_view id)
+{
+  auto it = std::find_if(std::begin(rooms), std::end(rooms), [id](const auto& r){
+    return r.id() == id;
+  });
+  if (it != std::end(rooms))
+    return &*it;
+  return nullptr;
+}
+
+
 }  // Anonymous namespace
 
 
